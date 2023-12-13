ed428cb8112abe32187772d49d5d6d770fc82b1d
Leon Bruns <ujiqk@student.kit.edu>
2023-12-13 18:38:33 +0100

    repaired gradle system

--- controller/src/main/java/edu/kit/riscjblockits/controller/blocks/IQueryableBlockEntity.java
+++ controller/src/main/java/edu/kit/riscjblockits/controller/blocks/IQueryableBlockEntity.java
@@ -1,5 +1,9 @@
 package edu.kit.riscjblockits.controller.blocks;
 
+import java.util.List;
+
 public interface IQueryableBlockEntity {
     public void setBlockModel();
+
+    List<BlockController> getComputerNeighbours();
 }
