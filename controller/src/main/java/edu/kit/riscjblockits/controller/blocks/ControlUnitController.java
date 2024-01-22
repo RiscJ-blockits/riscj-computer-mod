a1bab07a9cd1cb689094d615f2c2acc0b686fb6d
Leon Bruns <ujiqk@student.kit.edu>
2024-01-22 20:36:18 +0100

    ist model can be updated

--- controller/src/main/java/edu/kit/riscjblockits/controller/blocks/ControlUnitController.java
+++ controller/src/main/java/edu/kit/riscjblockits/controller/blocks/ControlUnitController.java
@@ -1,5 +1,6 @@
 package edu.kit.riscjblockits.controller.blocks;
 
+import edu.kit.riscjblockits.controller.clustering.ClusterHandler;
 import edu.kit.riscjblockits.model.blocks.ControlUnitModel;
 import edu.kit.riscjblockits.model.blocks.IControllerQueryableBlockModel;
 import edu.kit.riscjblockits.model.blocks.RegisterModel;
@@ -52,7 +53,8 @@ public class ControlUnitController extends ComputerBlockController{
          *                                  key: "foundMemory", value: string with number of memory blocks
          *                                  key: "foundAlu", value: string with number of alu blocks
          *                                  key: "foundControlUnit", value: string with number of control unit blocks
-         *                                  key: "foundControlUnit", value: string with number of control unit blocks
+         *                                  key: "foundSystemClock", value: string with number of system clock blocks
+         *              key: "istModel", value: ToDo
          */
         if (!data.isContainer()) {
             return;
@@ -60,10 +62,23 @@ public class ControlUnitController extends ComputerBlockController{
         for (String s : ((IDataContainer) data).getKeys()) {
             if (s.equals("clustering")) {
                 ((ControlUnitModel) getModel()).setClusteringData(((IDataContainer) data).get(s));
-            }
-            //ToDo add method to update Ist Model
-        }
+            } else if (s.equals("istModel")) {
+                //ToDo add method to update Ist Model
 
+            }
+        }
+    }
+
+    /**
+     * We need to override this method because we need to update the IstModel in the ClusterHandler when a new Cluster is created.
+     * @param clusterHandler The ClusterHandler that should be associated with the controller.
+     */
+    @Override
+    public void setClusterHandler(ClusterHandler clusterHandler) {
+        super.setClusterHandler(clusterHandler);
+        //if we have multiple control units, the fastest gets to set the IstModel
+        boolean success =  clusterHandler.setIstModel(((ControlUnitModel) getModel()).getIstModel());
+        //ToDo tell the player if this istModel is not the one that is used
     }
 
 }
