7077e09723f05c3eeba23bae0001ca7cdca54288
Nils Matthaeus <uplpy@student.kit.edu>
2024-01-28 17:35:41 +0100

    added block states and a hitbox to programming block

--- view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/programming/ProgrammingBlock.java
+++ view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/programming/ProgrammingBlock.java
@@ -2,9 +2,17 @@ package edu.kit.riscjblockits.view.main.blocks.mod.programming;
 
 import edu.kit.riscjblockits.view.main.blocks.mod.ModBlock;
 import edu.kit.riscjblockits.view.main.blocks.mod.computer.systemclock.SystemClockBlockEntity;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -43,4 +51,20 @@ public class ProgrammingBlock extends ModBlock {
         return new ProgrammingBlockEntity(pos, state);
     }
 
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(Properties.HORIZONTAL_FACING);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1f, 2f, 1f);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
 }
