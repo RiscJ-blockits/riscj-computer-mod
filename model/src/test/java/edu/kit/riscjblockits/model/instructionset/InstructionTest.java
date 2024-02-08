ba50d769d0fc12889d2580aa198f44620bdafc2c
Nils Matthaeus <uplpy@student.kit.edu>
2024-02-08 01:31:31 +0100

    fixed a bug in the instruction set and added some tests along the way

--- model/src/test/java/edu/kit/riscjblockits/model/instructionset/InstructionTest.java
+++ model/src/test/java/edu/kit/riscjblockits/model/instructionset/InstructionTest.java
@@ -34,4 +34,31 @@ class InstructionTest {
         assertEquals("0000", clonedInstruction.getExecution()[0].getTo());
         assertEquals("111111111111", clonedInstruction.getExecution()[1].getFrom()[0]);
     }
+
+    @Test
+    void testDetectionOfBinaryBGE() {
+        InstructionSetModel model = InstructionSetBuilder.buildInstructionSetModelRiscV();
+        Instruction instruction = model.getInstruction("bge");
+        assertTrue(instruction.matchesBinary("00000000010101000101000001100011"));
+    }
+
+    @Test
+    void testDetectionOfBinaryJAL() {
+        InstructionSetModel model = InstructionSetBuilder.buildInstructionSetModelRiscV();
+        Instruction instruction = model.getInstruction("jal");
+        assertTrue(instruction.matchesBinary("00100011010000000001001101101111"));
+    }
+
+    @Test
+    void testFillingBGE() {
+        InstructionSetModel model = InstructionSetBuilder.buildInstructionSetModelRiscV();
+        Instruction instruction = model.getInstruction("bge");
+        HashMap<Integer, String> intRegisters = new HashMap<>();
+        intRegisters.put(8, "s0");
+        intRegisters.put(5, "t0");
+        Instruction clonedInstruction = new Instruction(instruction, "00000000010101000101000001100011", intRegisters, new HashMap<>());
+        ConditionedInstruction conditionedInstruction = (ConditionedInstruction) clonedInstruction.getExecution()[3];
+        assertEquals("s0", conditionedInstruction.getCondition().getCompare1());
+        assertEquals("t0", conditionedInstruction.getCondition().getCompare2());
+    }
 }
