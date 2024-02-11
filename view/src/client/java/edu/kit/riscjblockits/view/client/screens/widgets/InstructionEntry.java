a03c5245bfe693481b0790a8940074194894a75c
Nicolas Wienges <nicolas.wienges@web.de>
2024-02-11 17:41:36 +0100

    Made the InstructionsWidget pretty *sparkle*. Added Tooltip funtionality for overflowing instructions, this is still buggy.

--- view/src/client/java/edu/kit/riscjblockits/view/client/screens/widgets/InstructionEntry.java
+++ view/src/client/java/edu/kit/riscjblockits/view/client/screens/widgets/InstructionEntry.java
@@ -3,6 +3,7 @@ package edu.kit.riscjblockits.view.client.screens.widgets;
 import edu.kit.riscjblockits.view.main.RISCJ_blockits;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
@@ -14,23 +15,52 @@ public class InstructionEntry extends ListEntry{
     private static final int OFFSET = 34;
     private final String arguments;
     private final String identifier;
+    private final InstructionListWidget parent;
 
-
-    public InstructionEntry(String identifier, String arguments) {
+    public InstructionEntry(String identifier, String arguments, InstructionListWidget parent) {
         this.arguments = arguments;
         this.identifier = identifier;
+        this.parent = parent;
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         MinecraftClient client = MinecraftClient.getInstance();
 
-        context.drawText(client.textRenderer, this.identifier, this.x + 1, this.y + 1, 0xffffff, false);
-        context.drawText(client.textRenderer, this.arguments, this.x + OFFSET, this.y + 1 , 0xffffff, false);
+        if(this.identifier.length() > 5){
+            String shortenedIdentifier = this.identifier.substring(0, 4) +"...";
+            context.drawText(client.textRenderer, shortenedIdentifier, this.x + 1, this.y + 1, 0xffffff, false);
+        }else {
+            context.drawText(client.textRenderer, this.identifier, this.x + 1, this.y + 1, 0xffffff, false);
+        }
+
+        if (this.arguments.length() > 15) {
+            String shortenedArguments = this.arguments.substring(0, 14) + "...";
+            context.drawText(client.textRenderer, shortenedArguments, this.x + OFFSET, this.y + 1, 0xffffff, false);
+        } else {
+            context.drawText(client.textRenderer, this.arguments, this.x + OFFSET, this.y + 1, 0xffffff, false);
+        }
+
+        context.disableScissor();
+        if(this.isMouseOver(mouseX, mouseY)) {
+            context.getMatrices().push();
+            context.getMatrices().translate(0.0f, 0.0f, 100.0f);
+
+            context.drawTooltip(client.textRenderer, Text.literal(this.identifier + ": " + this.arguments), mouseX, mouseY);
+
+            context.getMatrices().pop();
+        }
+        context.enableScissor(parent.getX(), parent.getY(), parent.getX() + parent.getWidth(), parent.getY() + parent.getHeight());
+
     }
 
     @Override
     public int getHeight() {
         return ENTRY_HEIGHT;
+    }
+
+    @Override
+    public int getWidth() {
+        return ENTRY_WIDTH;
     }
 }
