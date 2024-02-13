aca0e70fb3ccf872cd00b6c9070347f9dfc1a8e2
Nicolas Wienges <nicolas.wienges@web.de>
2024-02-13 14:42:22 +0100

    Programming Screen quick move added.

--- view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/programming/ProgrammingScreenHandler.java
+++ view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/programming/ProgrammingScreenHandler.java
@@ -171,4 +171,28 @@ public class ProgrammingScreenHandler extends ModScreenHandler {
 
         return instructionSet.getPossibleInstructions();
     }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int invSlot) {
+        ItemStack newStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(invSlot);
+        if (slot != null && slot.hasStack()) {
+            ItemStack originalStack = slot.getStack();
+            newStack = originalStack.copy();
+            if (invSlot < this.inventory.size()) {
+                if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
+                return ItemStack.EMPTY;
+            }
+            if (originalStack.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+        }
+        return newStack;
+    }
+
 }
