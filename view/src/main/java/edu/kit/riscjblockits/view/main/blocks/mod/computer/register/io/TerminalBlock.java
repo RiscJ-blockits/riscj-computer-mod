1fe442fd3c7b599d0ad7b2db3c64551217e93dc5
Nicolas Wienges <nicolas.wienges@web.de>
2024-03-07 01:24:27 +0100

    Made Terminal Block Directional

--- view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/computer/register/io/TerminalBlock.java
+++ view/src/main/java/edu/kit/riscjblockits/view/main/blocks/mod/computer/register/io/TerminalBlock.java
@@ -1,10 +1,14 @@
 package edu.kit.riscjblockits.view.main.blocks.mod.computer.register.io;
 
 import edu.kit.riscjblockits.view.main.blocks.mod.computer.register.RegisterBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -33,6 +37,23 @@ public class TerminalBlock extends RegisterBlock {
             }
         }
         return ActionResult.SUCCESS;
+    }
+
+    /**
+     * Adds custom block states to the block.
+     * The programming block has different textures on different sides.
+     * @param builder {@link net.minecraft.state.StateManager.Builder}
+     */
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(Properties.HORIZONTAL_FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing());
     }
 
 }
